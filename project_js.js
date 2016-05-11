201fa7766f4bd7263a40803adc6be5227ed13d9b
Tobias Urff <turff@ams-turff1-4.local>
2016-05-11 17:18:04 +0200

    Added Regus-specific url mappings

--- project_js.js
+++ project_js.js
@@ -30,9 +30,63 @@ window.optimizelyRedirect = {
 }
 
 optimizelyRedirect.url_mappings.push({
-  "origin": "urff.me/?(\\?.*)?$",
-  "destination": "http://tobiasurff.nl/"
+  "origin": "regus.co.uk/?(\\?.*)?$",
+  "destination": "http://b.regus.co.uk/"
 }, {
-  "origin": "/example/test.aspx",
-  "destination": "http://example.net/funky-example/funky-test"
+  "origin": "/products/meeting-facilities/meeting-rooms.aspx",
+  "destination": "http://b.regus.co.uk/meeting-rooms"
+}, {
+  "origin": "/products/virtual-offices/",
+  "destination": "http://b.regus.co.uk/virtual-office"
+}, {
+  "origin": "/products/virtual-offices/business-address.aspx",
+  "destination": "http://b.regus.co.uk/virtual-office"
+}, {
+  "origin": "/office-space/united-kingdom/london",
+  "destination": "http://b.regus.co.uk/office-space/united-kingdom/london"
+}, {
+  "origin": "/meeting-rooms/united-kingdom/london",
+  "destination": "http://b.regus.co.uk/meeting-rooms/united-kingdom/london"
+}, {
+  "origin": "/products/offices/index.aspx",
+  "destination": "http://b.regus.co.uk/office-space"
+}, {
+  "origin": "/locations/office-space/london-city-point",
+  "destination": "http://b.regus.co.uk/office-space/united-kingdom/london/london-city-point"
+}, {
+  "origin": "/locations/office-space/london-tower-42",
+  "destination": "http://b.regus.co.uk/office-space/united-kingdom/london/london-tower-42"
+}, {
+  "origin": "/virtual-office/united-kingdom/london",
+  "destination": "http://b.regus.co.uk/virtual-office/united-kingdom/london"
+}, {
+  "origin": "/products/virtual-offices/mail-handling.aspx",
+  "destination": "http://b.regus.co.uk/virtual-office"
+}, {
+  "origin": "/products/offices/office-space-to-rent.aspx",
+  "destination": "http://b.regus.co.uk/office-space"
+}, {
+  "origin": "/locations/office-space/london-berkeley-square",
+  "destination": "http://b.regus.co.uk/virtual-office/united-kingdom/london/london-berkeley-square"
+}, {
+  "origin": "/locations/office-space/london-fleet-street",
+  "destination": "http://b.regus.co.uk/virtual-office/united-kingdom/london/london,-fleet-street"
+}, {
+  "origin": "/products/businessworld-membership/index.aspx",
+  "destination": "http://b.regus.co.uk/virtual-office"
+}, {
+  "origin": "/locations/office-space/manchester-peter-house",
+  "destination": "http://b.regus.co.uk/virtual-office/united-kingdom/manchester/manchester-peter-house"
+}, {
+  "origin": "/products/business-lounges/",
+  "destination": "http://b.regus.co.uk/virtual-office"
+}, {
+  "origin": "/locations/office-space/london-new-broad-street",
+  "destination": "http://b.regus.co.uk/office-space/united-kingdom/london/london-new-broad-street"
+}, {
+  "origin": "/locations/office-space/london-victoria-greycoat-place",
+  "destination": "http://b.regus.co.uk/office-space/united-kingdom/london/london,-victoria---greycoat-place"
+}, {
+  "origin": "cat=brand",
+  "destination": "http://b.regus.co.uk/"
 });
