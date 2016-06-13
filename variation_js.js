f0537843cdcd12aee4d2fdb60c493b88c3d23c75
Tobi Urff <tobias@urff.at>
2016-06-13 14:20:05 +0200

    Made pseudo comment clearer

--- variation_js.js
+++ variation_js.js
@@ -1,2 +1,2 @@
-/* _optimizely_redirect=http://example.com/ */
+/* _optimizely_redirect=http://example.com/this-url-gets-loaded-in-the-optimizely-editor-as-preview */
 optimizelyRedirect.redirectToMappedUrl(window.location.href);
